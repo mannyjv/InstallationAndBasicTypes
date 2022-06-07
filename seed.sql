23458d813e287714cefa7c7abe8557c00cb15d15
Emmanuel Vazquez <emmanuel.vazquez06@gmail.com>
2022-06-07 20:01:19 +0200

    Refactor: sql seeding
    Adds a script to package.json to seed database for demo. Added more dummy data to seed with.

---
+++ seed.sql
@@ -0,0 +1,43 @@
+DROP TABLE IF EXISTS argonautes;
+
+CREATE TABLE argonautes (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(25) NOT NULL
+);
+
+INSERT INTO argonautes (name)
+VALUES
+('Eleftheria'),
+('Gennadios'),
+('Lysimachos'),
+('Eleftheria'),
+('Gennadios'),
+('Lysimachos'),
+('Eleftheria'),
+('Gennadios'),
+('Lysimachos'),
+('Eleftheria'),
+('Gennadios'),
+('Lysimachos'),
+('Eleftheria'),
+('Gennadios'),
+('Lysimachos'),
+('Eleftheria'),
+('Gennadios'),
+('Lysimachos'),
+('Eleftheria'),
+('Gennadios'),
+('Lysimachos'),
+('Eleftheria'),
+('Gennadios'),
+('Lysimachos'),
+('Eleftheria'),
+('Gennadios'),
+('Lysimachos'),
+('Eleftheria'),
+('Gennadios'),
+('Lysimachos'),
+('Eleftheria'),
+('Eleftheria'),
+('Gennadios');
+
